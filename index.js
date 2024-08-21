b99e81ef051e28cf1b03e3de6edabc8805c389c9
bijen maharjan <bijenmaharjan55@gmail.com>
2024-08-21 22:03:19 +0545

    Creating the textfile with routing

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-app.set('views engine', 'ejs');
 
 
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir(`./files`, (err, file) => {
+    res.render("index", { files: file });
+
+  })
+
 });
+app.post("/create", (req, res) => {
+  fs.writeFile(`./files/${req.body.title.split(' ').join(' ')}.txt`, req.body.details, (err) => {
+
+    res.redirect("/");
+  })
+
+})
+
+
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render('show', { filename: req.params.filename, filedata: filedata });
+
+  })
+})
 
 app.get("/profile/:username", (req, res) => {
   res.send(`welcome,${req.params.username}`);
@@ -22,6 +43,7 @@ app.get("/profile/:username", (req, res) => {
 app.get("/profile/:username/:age", (req, res) => {
   res.send(`welcome,${req.params.username} of age ${req.params.age}`);
 });
+
 
 
 
